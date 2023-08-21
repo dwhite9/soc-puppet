240e5d25c7db41c5028dc8171094b4513da68bbe
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2023-08-21 16:25:10 -0400

    user management detections added

--- socpuppet/detections/sentinel/__init__.py
+++ socpuppet/detections/sentinel/__init__.py
@@ -1,2 +1,3 @@
 from socpuppet.detections.sentinel.audit_log_role_management import *
 from socpuppet.detections.sentinel.audit_log_domain_trust import *
+from socpuppet.detections.sentinel.audit_log_user_management import *
