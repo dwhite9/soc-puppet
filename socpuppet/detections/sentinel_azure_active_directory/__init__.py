cf72bfda534b61bb86f7a130ddbe3a3ecb5827cc
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2023-08-22 11:10:07 -0400

    Create __init__.py

---
+++ socpuppet/detections/sentinel_azure_active_directory/__init__.py
@@ -0,0 +1,4 @@
+from socpuppet.detections.sentinel_azure_active_directory.audit_log_role_management import *
+from socpuppet.detections.sentinel_azure_active_directory.audit_log_policy import *
+from socpuppet.detections.sentinel_azure_active_directory.audit_log_user_management import *
+from socpuppet.detections.sentinel_azure_active_directory.audit_log_groupmanagement import *
