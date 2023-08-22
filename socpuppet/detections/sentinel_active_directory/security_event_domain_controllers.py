e763d3614c3ef456c9814a6e8fd0a5d7219b3b28
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2023-08-22 11:09:55 -0400

    Update security_event_domain_controllers.py

--- socpuppet/detections/sentinel_active_directory/security_event_domain_controllers.py
+++ socpuppet/detections/sentinel_active_directory/security_event_domain_controllers.py
@@ -1,16 +1,17 @@
 
 
-def function():
+def ad_4625_threshold_match(x: str, y: int):
     logic_json = {
-        "query": """
+        "query": f"""
         SecurityEvent
+        | where TimeGenerated >= ago({x})
         | where EventID == 4625 
             and AccountType == 'User' 
             and SubStatus == '0x0000064'
         | summarize AccountSet=make_set(TargetUserName) by IpAddress, SubStatus, EventID, 
             TargetDomainName, bin(TimeGenerated, 1h)
         | extend TargetUserNameCount = array_length(AccountSet)
-        | where TargetUserNameCount >= 5 
+        | where TargetUserNameCount >= {y} 
         """,
         "title": "4625(F) User Logon with Bad Account Name from Same IP ",
         "attackId": "T110.003",
@@ -18,7 +19,7 @@ def function():
         "platform": "Active_Directory",
         "deployGroup": "Domain_Controllers_All",
         "author": "Detection_Engineering",
-        "detectCon": "2",
+        "detectCon": "3",
         "type": "aggregate_threshold_match",
         "openSource": [],
         "emulation": [],
@@ -32,4 +33,68 @@ def function():
     return logic_json
 
 
+def ad_4768_threshold_match(x: str, y: int):
+    logic_json = {
+        "query": f"""
+        SecurityEvent
+        | where TimeGenerated >= ago({x})
+        | where EventID == 4768
+        | where TargetUserName !endswith '$' //Remove Machine Accounts
+        | extend IpExtract = extract(@'([0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3})' , 0 , IpAddress) 
+        | where Status in~ ('0x6')
+        | summarize AccountSet=make_set(TargetUserName) by IpExtract, ServiceName, Status, EventID, bin(TimeGenerated, 1h)
+        | extend AccountTotal = array_length(AccountSet)
+        | where AccountTotal >= {y} 
+        """,
+        "title": "4768(S,F) Auth With Bad User Names From Single IP",
+        "attackId": "T110.003",
+        "dataSource": "Security_Event_Log",
+        "platform": "Active_Directory",
+        "deployGroup": "Domain_Controllers_All",
+        "author": "Detection_Engineering",
+        "detectCon": "3",
+        "type": "aggregate_threshold_match",
+        "openSource": [],
+        "emulation": [],
+        "intelReference": [],
+        "generalReference": [],
+        "tags": [],
+        "responsePlaybook": [],
+        "detectUseCases": [],
+    }
+
+    return logic_json
+
+
+def ad_4771_threshold_match(x: str, y: int):
+    logic_json = {
+        "query": f"""
+        SecurityEvent
+        | where TimeGenerated >= ago({x})
+        | where EventID == 4771
+        | where TargetUserName !endswith '$' //Remove Machine Accounts
+        | extend IpExtract = extract(@'([0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3})' , 0 , IpAddress) 
+        | where Status in~ ('0x18') 
+        | summarize AccountSet=make_set(TargetUserName) by IpExtract, ServiceName, Status, EventID, bin(TimeGenerated, 1h)
+        | extend AccountTotal = array_length(AccountSet)
+        | where AccountTotal >= {y}
+        """,
+        "title": "4771(F) Wrong Password From Single IP",
+        "attackId": "T110.003",
+        "dataSource": "Security_Event_Log",
+        "platform": "Active_Directory",
+        "deployGroup": "Domain_Controllers_All",
+        "author": "Detection_Engineering",
+        "detectCon": "3",
+        "type": "aggregate_threshold_match",
+        "openSource": [],
+        "emulation": [],
+        "intelReference": [],
+        "generalReference": [],
+        "tags": [],
+        "responsePlaybook": [],
+        "detectUseCases": [],
+    }
+
+    return logic_json
 
