58c256f36b35e981acd0f732963ca4a32c985c61
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2023-08-22 13:01:33 -0400

    update

---
+++ socpuppet/detections/sentinel/__init__.py
@@ -0,0 +1,11 @@
+from socpuppet.detections.sentinel.active_directory.security_event_domain_controllers import *
+
+from socpuppet.detections.sentinel.azure_active_directory.audit_log_application_management import *
+from socpuppet.detections.sentinel.azure_active_directory.audit_log_group_management import *
+from socpuppet.detections.sentinel.azure_active_directory.audit_log_policy import *
+from socpuppet.detections.sentinel.azure_active_directory.audit_log_role_management import *
+from socpuppet.detections.sentinel.azure_active_directory.audit_log_user_management import *
+
+from socpuppet.detections.sentinel.google_workspace.admin_activity_delegated_admin import *
+from socpuppet.detections.sentinel.google_workspace.admin_activity_domain import *
+from socpuppet.detections.sentinel.google_workspace.admin_activity_user import *
--- socpuppet/detections/sentinel/azure_active_directory/audit_log_application_management.py
+++ socpuppet/detections/sentinel/azure_active_directory/audit_log_application_management.py
@@ -23,8 +23,67 @@ def aad_app_privileged_perms(x: str):
             'DelegatedPermissionGrant.ReadWrite.All')
         """,
         "title": "Specific API Perms Added To Service Principal",
-        "attackId": "T1098.003",
-        "dataSource": "Role_Management",
+        "attackId": "",
+        "dataSource": "Application_Management",
+        "platform": "Azure_Active_Directory",
+        "deployGroup": "AAD_All",
+        "author": "Detection_Engineering",
+        "detectCon": "4",
+        "type": "simple_pattern_match",
+        "openSource": [],
+        "emulation": [],
+        "intelReference": [],
+        "generalReference": [],
+        "tags": [],
+        "responsePlaybook": [],
+        "detectUseCases": [],
+    }
+
+    return logic_json
+
+
+def aad_app_mail_read_write(x: str):
+    logic_json = {
+        "query": f"""
+        AuditLogs
+        | where TimeGenerated >= ago({x})
+        | where Category =~ "ApplicationManagement"
+        | where OperationName has_any ("Add delegated permission grant", "Add app role assignment to service principal")
+        | where Result =~ "success"
+        | where TargetResources has_any ('mail.read', 'mail.readwrite', 'MailboxSettings', 'Mail.Send' , 'Mail')
+        """,
+        "title": "Mail Or Mailbox Read/Write Added To Service Principal",
+        "attackId": "",
+        "dataSource": "Application_Management",
+        "platform": "Azure_Active_Directory",
+        "deployGroup": "AAD_All",
+        "author": "Detection_Engineering",
+        "detectCon": "4",
+        "type": "simple_pattern_match",
+        "openSource": [],
+        "emulation": [],
+        "intelReference": [],
+        "generalReference": [],
+        "tags": [],
+        "responsePlaybook": [],
+        "detectUseCases": [],
+    }
+
+    return logic_json
+
+
+def aad_app_admin_consent(x: str):
+    logic_json = {
+        "query": f"""
+        AuditLogs
+        | where TimeGenerated >= ago({x})
+        | where Category =~ "ApplicationManagement"
+        | where OperationName =~ "Consent to application"
+        | where Result =~ 'success'
+        """,
+        "title": "Admin Consent Added To Service Principal",
+        "attackId": "",
+        "dataSource": "Application_Management",
         "platform": "Azure_Active_Directory",
         "deployGroup": "AAD_All",
         "author": "Detection_Engineering",
