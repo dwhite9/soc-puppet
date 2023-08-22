481b5da3ece7e0d37979cef5e7ce7db943005967
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2023-08-22 08:12:38 -0400

    Update __init__.py

--- socpuppet/detections/sentinel/__init__.py
+++ socpuppet/detections/sentinel/__init__.py
@@ -1,3 +1,4 @@
 from socpuppet.detections.sentinel.audit_log_role_management import *
-from socpuppet.detections.sentinel.audit_log_domain_trust import *
+from socpuppet.detections.sentinel.audit_log_policy import *
 from socpuppet.detections.sentinel.audit_log_user_management import *
+from socpuppet.detections.sentinel.audit_log_groupmanagement import *
