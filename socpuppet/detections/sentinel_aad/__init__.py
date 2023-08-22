d1feff1d7ea229fef4867ca9f50de511f4f72807
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2023-08-22 11:10:05 -0400

    refactor name

--- socpuppet/detections/sentinel_aad/__init__.py
+++
@@ -1,4 +0,0 @@
-from socpuppet.detections.sentinel_aad.audit_log_role_management import *
-from socpuppet.detections.sentinel_aad.audit_log_policy import *
-from socpuppet.detections.sentinel_aad.audit_log_user_management import *
-from socpuppet.detections.sentinel_aad.audit_log_groupmanagement import *
